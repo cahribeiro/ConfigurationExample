9c93efc01111757043dd88cda09de3994f93c54e
Carolina Ribeiro <carolina.santos000@gmail.com>
2022-03-14 11:18:50 +0000

    Add project files.

---
+++ ConfigurationExample/Controllers/ExampleController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace ConfigurationExample.Controllers
+{
+    public class ExampleController : Controller
+    {
+        private readonly ExampleSettings _settings;
+
+        public ExampleController(IOptions<ExampleSettings> settings)
+        {
+            _settings = settings.Value;
+        }
+
+        [HttpGet("example")]
+        public IActionResult Index()
+        {
+            return Ok(new
+            {
+                fromOption = _settings.ConnectionString
+            });
+        }
+    }
+}
---
+++ ConfigurationExample/ExampleSettings.cs
@@ -0,0 +1,4 @@
+﻿public class ExampleSettings
+{
+    public string ConnectionString { get; set; }
+}
